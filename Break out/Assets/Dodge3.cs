c73ef5ed7bc7ff076a624ccaabf8680f76a21935
AlexHasDogPie <abecerra5569@gmail.com>
2015-10-21 10:53:31 -0500

    Alex's Breakout Game

---
+++ Break out/Assets/Dodge3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dodge3 : MonoBehaviour {
+	public Vector3 movementDirection;
+	
+	void Start()
+	{
+		movementDirection = Vector3.left;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//float obMoveHorizontal = 5F;
+		//Vector3 obMovement = new Vector3 (obMoveHorizontal, 0.0f, 0.0f);
+		//transform.Rotate (new Vector3 (15,30,45) * Time.deltaTime);  
+		//Vector3 movementDirection = Vector3.left;
+		
+		if (transform.position.x < -1)
+			movementDirection = Vector3.right;
+		
+		else if (transform.position.x > 1)
+			movementDirection = Vector3.left;
+		
+		transform.Translate (movementDirection * Time.deltaTime * 10);
+	}
+	
+}
---
+++ Break out/Assets/Scripts/Deathzone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deathzone : MonoBehaviour {
+	
+	void OnTriggerEnter (Collider col)
+	{
+		GM.instance.LoseLife();
+	}
+}
